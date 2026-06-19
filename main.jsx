555aec6985e554855bf170edac48048bbac51893
stephenczinn-design <stephenczinn@gmail.com>
2026-06-19 14:21:47 -0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// This fixes the "React is not defined" error globally
+window.React = React
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
